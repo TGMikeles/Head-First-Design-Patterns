298a381e4d0878e5bbad25f7d0a7ab5b8b5894bd
TGMikeles <ikeles@students.tgm.ac.at>
2016-06-05 17:47:07 +0200

    Mallard Duck Java Doc

--- src/headfirst/designpatterns/combining/observer/MallardDuck.java
+++ src/headfirst/designpatterns/combining/observer/MallardDuck.java
@@ -1,25 +1,29 @@
 package headfirst.designpatterns.combining.observer;
-
+/*
+ *Die Klasse MallardDuck implementiert Quackable diese Klasse gibt der MallardDuck die Eigenschaften
+ *  Im Prinzip ist die Klasse sozusagen ein QuackObservable aber benimmt sich wie ein Observer
+ * @author Ibrahim Keles
+ */
 public class MallardDuck implements Quackable {
 	Observable observable;
- 
+
 	public MallardDuck() {
 		observable = new Observable(this);
 	}
- 
+
 	public void quack() {
 		System.out.println("Quack");
 		notifyObservers();
 	}
- 
+
 	public void registerObserver(Observer observer) {
 		observable.registerObserver(observer);
 	}
- 
+
 	public void notifyObservers() {
 		observable.notifyObservers();
 	}
- 
+
 	public String toString() {
 		return "Mallard Duck";
 	}
