2b9013c9f0e9726e8abecb1d63b25e4c799a100f
TGMikeles <ikeles@students.tgm.ac.at>
2016-06-05 17:15:03 +0200

    Quackologistig mit Java Docs

--- src/headfirst/designpatterns/combining/observer/Quackologist.java
+++ src/headfirst/designpatterns/combining/observer/Quackologist.java
@@ -1,11 +1,14 @@
 package headfirst.designpatterns.combining.observer;
-
+/*
+ * Die Klasse  implementier das Interface Observer
+ *@author Ibrahim Keles
+ */
 public class Quackologist implements Observer {
  
 	public void update(QuackObservable duck) {
 		System.out.println("Quackologist: " + duck + " just quacked.");
 	}
- 
+
 	public String toString() {
 		return "Quackologist";
 	}
