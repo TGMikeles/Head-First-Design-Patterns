dc31eb449ed69ec9595259c6e7ba1155aadd4c0d
TGMikeles <ikeles@students.tgm.ac.at>
2016-06-05 16:51:06 +0200

    Quack Observerable mit java doc

--- src/headfirst/designpatterns/combining/observer/QuackObservable.java
+++ src/headfirst/designpatterns/combining/observer/QuackObservable.java
@@ -1,4 +1,10 @@
 package headfirst.designpatterns.combining.observer;
+/*
+ * Das Interface QuackObservable ist dazu da damit sich ein
+ * Observer so zu sagen regestrieren kann somit kann er informationen bekommen.
+ * Sobald es eine Ändereung gibt wird es mit NotifyObserver eine Information gegeben
+ * @author Ibrahim Keles
+ */
 
 public interface QuackObservable {
 	public void registerObserver(Observer observer);
