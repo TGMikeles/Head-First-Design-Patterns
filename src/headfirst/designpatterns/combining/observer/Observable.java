97ec3b7bcb23ad5e5cd340e71abb849bc5272f1c
TGMikeles <ikeles@students.tgm.ac.at>
2016-06-05 17:28:03 +0200

    Javadoc von Observer

--- src/headfirst/designpatterns/combining/observer/Observable.java
+++ src/headfirst/designpatterns/combining/observer/Observable.java
@@ -2,19 +2,24 @@ package headfirst.designpatterns.combining.observer;
 
 import java.util.Iterator;
 import java.util.ArrayList;
-
+/*
+ * Die Klasse Observer implementiert das interface QuackObservable
+ *somit ist es möglich die Methoden des Interface zu benutzen.
+ * Im Prinzip ist die Klasse sozusagen ein QuackObservable aber benimmt sich wie ein Observer
+ * @author Ibrahim Keles
+ */
 public class Observable implements QuackObservable {
 	ArrayList<Observer> observers = new ArrayList<Observer>();
 	QuackObservable duck;
- 
+
 	public Observable(QuackObservable duck) {
 		this.duck = duck;
 	}
-  
+
 	public void registerObserver(Observer observer) {
 		observers.add(observer);
 	}
-  
+
 	public void notifyObservers() {
 		Iterator<Observer> iterator = observers.iterator();
 		while (iterator.hasNext()) {
@@ -22,7 +27,7 @@ public class Observable implements QuackObservable {
 			observer.update(duck);
 		}
 	}
- 
+
 	public Iterator<Observer> getObservers() {
 		return observers.iterator();
 	}
