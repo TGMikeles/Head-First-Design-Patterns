4e2cab80a88b8b15c6252152e7c5bd1439f45eb6
TGMikeles <ikeles@students.tgm.ac.at>
2016-06-05 16:58:37 +0200

    Quackable mit Javadoc

--- src/headfirst/designpatterns/combining/observer/Quackable.java
+++ src/headfirst/designpatterns/combining/observer/Quackable.java
@@ -1,4 +1,9 @@
 package headfirst.designpatterns.combining.observer;
+/*
+ *Das Interface Quackable erbt von dem Interface QuackObservable
+ *somit hat auch dieses Interface die Methodenköpfe von dem diesem Interface.
+ * @author Ibrahim Keles
+ */
 
 public interface Quackable extends QuackObservable {
 	public void quack();
