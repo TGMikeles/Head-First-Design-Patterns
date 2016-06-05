36be8ea9a9c310d7611391a88bc975689bbfe66e
TGMikeles <ikeles@students.tgm.ac.at>
2016-06-05 17:48:19 +0200

    Rubber Duck Java Doc

--- src/headfirst/designpatterns/combining/observer/RubberDuck.java
+++ src/headfirst/designpatterns/combining/observer/RubberDuck.java
@@ -1,12 +1,16 @@
 package headfirst.designpatterns.combining.observer;
-
+/*
+ *Die Klasse RubberDuck implementiert Quackable diese Klasse gibt der RubberDuck die Eigenschaften
+ *  Im Prinzip ist die Klasse sozusagen ein QuackObservable aber benimmt sich wie ein Observer
+ * @author Ibrahim Keles
+ */
 public class RubberDuck implements Quackable {
 	Observable observable;
 
 	public RubberDuck() {
 		observable = new Observable(this);
 	}
- 
+
 	public void quack() {
 		System.out.println("Squeak");
 		notifyObservers();
@@ -19,7 +23,7 @@ public class RubberDuck implements Quackable {
 	public void notifyObservers() {
 		observable.notifyObservers();
 	}
-  
+
 	public String toString() {
 		return "Rubber Duck";
 	}
