71e19fa5b03e8ca5bd4e1324164966b413f78fb5
TGMikeles <ikeles@students.tgm.ac.at>
2016-06-05 17:39:23 +0200

    JavaDoc von RedheadDuck

--- src/headfirst/designpatterns/combining/observer/RedheadDuck.java
+++ src/headfirst/designpatterns/combining/observer/RedheadDuck.java
@@ -1,5 +1,9 @@
 package headfirst.designpatterns.combining.observer;
-
+/*
+ *Die Klasse RedheadDuck implementiert Quackable diese Klasse gibt der RedheadDuck die Eigenschaften
+ *  Im Prinzip ist die Klasse sozusagen ein QuackObservable aber benimmt sich wie ein Observer
+ * @author Ibrahim Keles
+ */
 public class RedheadDuck implements Quackable {
 	Observable observable;
 
