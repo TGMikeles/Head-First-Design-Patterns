1d5709eaffa59c55a2c0ac529315dd687fa0d006
TGMikeles <ikeles@students.tgm.ac.at>
2016-06-05 18:00:09 +0200

    QuackCounter Java Doc

--- src/headfirst/designpatterns/combining/observer/QuackCounter.java
+++ src/headfirst/designpatterns/combining/observer/QuackCounter.java
@@ -1,30 +1,35 @@
 package headfirst.designpatterns.combining.observer;
-
+/*
+ *Die Klasse QuackCounter implementiert das Interface Quackable.
+ * Diese Klasse ist dazu da damit man die Anzahl des Quckens zähllt und sie ausgibt
+ *  Im Prinzip ist die Klasse sozusagen ein QuackObservable aber benimmt sich wie ein Observer
+ * @author Ibrahim Keles
+ */
 public class QuackCounter implements Quackable {
 	Quackable duck;
 	static int numberOfQuacks;
-  
+
 	public QuackCounter(Quackable duck) {
 		this.duck = duck;
 	}
-  
+
 	public void quack() {
 		duck.quack();
 		numberOfQuacks++;
 	}
- 
+
 	public static int getQuacks() {
 		return numberOfQuacks;
 	}
- 
+
 	public void registerObserver(Observer observer) {
 		duck.registerObserver(observer);
 	}
- 
+
 	public void notifyObservers() {
 		duck.notifyObservers();
 	}
-   
+
 	public String toString() {
 		return duck.toString();
 	}
