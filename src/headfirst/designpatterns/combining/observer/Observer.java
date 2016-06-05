5d1842255f5982488d638bb6aa950d0fd91fe25c
TGMikeles <ikeles@students.tgm.ac.at>
2016-06-05 17:07:15 +0200

    Observer mit der Javadoc

--- src/headfirst/designpatterns/combining/observer/Observer.java
+++ src/headfirst/designpatterns/combining/observer/Observer.java
@@ -1,5 +1,11 @@
 package headfirst.designpatterns.combining.observer;
-
+/*
+ * Das Interface Observer halt als Parameter QuackObservable das heißt das er die Methoden
+ * aus diesen Interface nutzen kann wie zum Beispiel registerObserver() oder notifyObservers()
+ *somit kann er in der Methode update alle Information ausgeben die er von  QuackObservable bekommt
+ *@author Ibrahim Keles
+ *@param QuackObservable: duck
+ */
 public interface Observer {
 	public void update(QuackObservable duck);
 }
