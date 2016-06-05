09862246f5595082df66fca44c19ae28313d6d18
TGMikeles <ikeles@students.tgm.ac.at>
2016-06-05 18:05:12 +0200

    Goose Adapter Java Doc

--- src/headfirst/designpatterns/combining/observer/GooseAdapter.java
+++ src/headfirst/designpatterns/combining/observer/GooseAdapter.java
@@ -1,5 +1,10 @@
 package headfirst.designpatterns.combining.observer;
-
+/*
+ * Die Klasse GooseAdapter implementiert Quackable
+ * Außerdem hat eine Referenz auf Goose damit sie sozusagen ihre eigene Eigenschaften hat mit dem Honk 
+ * Im Prinzip ist die Klasse sozusagen ein QuackObservable aber benimmt sich wie ein Observer
+ * @author Ibrahim Keles
+ */
 public class GooseAdapter implements Quackable {
 	Goose goose;
 	Observable observable;
@@ -8,7 +13,7 @@ public class GooseAdapter implements Quackable {
 		this.goose = goose;
 		observable = new Observable(this);
 	}
- 
+
 	public void quack() {
 		goose.honk();
 		notifyObservers();
